6e35b873dc7f7e41b4ab96c6bf7c17766d535c6d
Aleen <aleen42@vip.qq.com>
2019-06-06 16:00:54 +0800

    release(es3-compatible-webpack-plugin): bump version to 1.0.5

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -24,15 +26,23 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
  *  - Author: aleen42
  *  - Description:
  *  - Create Time: Feb, 9th, 2018
- *  - Update Time: Feb, 24th, 2018
+ *  - Update Time: Jun, 6th, 2019
  *
  */
 
 var SourceMapSource = require('webpack-core/lib/SourceMapSource');
 
-var UglifyJS = require('uglify-js');
+var _require = require('uglify-js'),
+    AST_ObjectProperty = _require.AST_ObjectProperty,
+    AST_Dot = _require.AST_Dot,
+    AST_Array = _require.AST_Array,
+    AST_Object = _require.AST_Object,
+    TreeWalker = _require.TreeWalker,
+    parse = _require.parse;
 
 /** EMCAScript-262 */
+
+
 var KEYWORDS = ['break', 'do', 'instanceof', 'typeof', 'case', 'else', 'new', 'var', 'catch', 'finally', 'return', 'void', 'continue', 'for', 'switch', 'while', 'debugger', 'function', 'this', 'with', 'default', 'if', 'throw', 'delete', 'in', 'try'];
 var RESERVED_WORDS = ['abstract', 'boolean', 'byte', 'char', 'class', 'double', 'enum', 'export', 'extends', 'final', 'float', 'goto', 'implements', 'import', 'int', 'interface', 'long', 'native', 'package', 'private', 'protected', 'public', 'short', 'static', 'super', 'synchronized', 'this', 'throws', 'transient', 'volatile'];
 var NULL_LITERAL = ['null'];
@@ -60,25 +70,33 @@ var ES3CompatiblePlugin = function () {
         key: 'process',
         value: function process(source) {
             /** the AST structure (http://lisperator.net/uglifyjs/ast) of UglifyJS */
-            var ast = UglifyJS.parse(source);
+            var ast = parse(source);
 
             /** code snippets to be replaced */
             var snippets = [];
+            var _snippet = function _snippet(type, key, start, end) {
+                return snippets.push({ type: type, key: key, start: start, end: end });
+            };
+            var _substr = source.substring.bind(source);
 
-            ast.walk(new UglifyJS.TreeWalker(function (node) {
-                if (node instanceof UglifyJS.AST_ObjectProperty && new RegExp(literalRegex, 'g').test(node.key)) {
+            ast.walk(new TreeWalker(function (node) {
+                if (node instanceof AST_ObjectProperty && new RegExp(literalRegex, 'g').test(node.key)) {
+                    var key = node.key,
+                        _node$start = node.start,
+                        pos = _node$start.pos,
+                        endpos = _node$start.endpos;
                     /** object properties defined by reserved words */
-                    snippets.push({
-                        type: 'property_definition',
-                        key: node.key,
-                        start: node.start.pos,
-                        end: node.start.endpos
-                    });
-                } else if (node instanceof UglifyJS.AST_Dot && new RegExp(literalRegex, 'g').test(node.property)) {
+
+                    _snippet('property_definition', key, pos, endpos);
+                } else if (node instanceof AST_Dot && new RegExp(literalRegex, 'g').test(node.property)) {
                     /** access property defined by reserved words */
-                    var snippetObject = { type: 'dot_access', key: node.property };
-                    if (source.substring(node.end.pos, node.end.endpos) === node.property) {
-                        snippets.push(Object.assign(snippetObject, { start: node.end.pos, end: node.end.endpos }));
+                    if (_substr(node.end.pos, node.end.endpos) === node.property) {
+                        var property = node.property,
+                            _node$end = node.end,
+                            _pos = _node$end.pos,
+                            _endpos = _node$end.endpos;
+
+                        _snippet('dot_access', property, _pos, _endpos);
                     } else {
                         /**
                          * node.start.pos
@@ -87,38 +105,30 @@ var ES3CompatiblePlugin = function () {
                          *    \      /
                          * if (a.b.c), then both start and end are token of parentheses
                          */
-                        var wholeString = source.substring(node.start.pos, node.end.endpos);
+                        var _property = node.property,
+                            start = node.start,
+                            end = node.end;
 
-                        snippets.push(Object.assign(snippetObject, {
-                            start: node.start.pos + wholeString.lastIndexOf(snippetObject.key),
-                            end: node.start.pos + wholeString.lastIndexOf(snippetObject.key) + snippetObject.key.length
-                        }));
+                        var entire = _substr(start.pos, end.endpos);
+
+                        _snippet('dot_access', _property, start.pos + entire.lastIndexOf(_property), start.pos + entire.lastIndexOf(_property) + _property.length);
                     }
-                } else if (node instanceof UglifyJS.AST_Array) {
-                    if (node.elements.length && source.substring(node.elements[node.elements.length - 1].end.endpos, node.end.endpos).indexOf(',') > -1) {
-                        /** array trailing comma */
+                } else if (node instanceof AST_Array || node instanceof AST_Object) {
+                    var elements = node instanceof AST_Array ? node.elements : node.properties;
+                    var lastElement = elements[elements.length - 1];
+
+                    /** trailing comma in Array or Object */
+                    if (elements.length && _substr(lastElement.end.endpos, node.end.endpos).indexOf(',') > -1) {
                         if (!node.end.comments_before.length) {
                             /** without comments before */
-                            snippets.push({
-                                type: 'array_trailing_comma',
-                                start: node.elements[node.elements.length - 1].end.endpos,
-                                end: node.end.endpos
-                            });
+                            _snippet('trailing_comma', '', lastElement.end.endpos, node.end.endpos);
                         } else {
                             /** between last item and comments */
-                            snippets.push({
-                                type: 'array_trailing_comma',
-                                start: node.elements[node.elements.length - 1].end.endpos,
-                                end: node.end.comments_before[0].pos
-                            });
+                            _snippet('trailing_comma', '', lastElement.end.endpos, node.end.comments_before[0].pos);
 
-                            if (source.substring(node.end.comments_before[0].endpos, node.end.endpos).indexOf(',') > -1) {
+                            if (_substr(node.end.comments_before[0].endpos, node.end.endpos).indexOf(',') > -1) {
                                 /** between comments and right-brackets */
-                                snippets.push({
-                                    type: 'array_trailing_comma',
-                                    start: node.end.comments_before[0].endpos,
-                                    end: node.end.endpos
-                                });
+                                _snippet('trailing_comma', '', node.end.comments_before[0].endpos, node.end.endpos);
                             }
                         }
                     }
@@ -137,38 +147,46 @@ var ES3CompatiblePlugin = function () {
             /** sort for nested dot accessing like "a.b.c.d" */
             return snippets.sort(function (prevItem, item) {
                 return prevItem.start - item.start;
-            }).reduce(function (result, item) {
-                var startIndex = lastIndex;
-                lastIndex = item.end;
+            }).map(function (_ref) {
+                var type = _ref.type,
+                    key = _ref.key,
+                    start = _ref.start,
+                    end = _ref.end;
 
-                switch (item.type) {
-                    case 'property_definition':
-                        return result + source.substring(startIndex, item.start) + source.substring(item.start, item.end).replace(new RegExp('^' + _escapeRegular(item.key) + '$', 'gi'), '\'' + _escapeReplacement(item.key) + '\'');
-                    case 'dot_access':
-                        return result + source.substring(startIndex, item.start - 1) + source.substring(item.start - 1, item.end).replace(new RegExp('^\\.' + _escapeRegular(item.key) + '$', 'gi'), '[\'' + _escapeReplacement(item.key) + '\']');
-                    case 'array_trailing_comma':
-                        return result + source.substring(startIndex, item.start) + source.substring(item.start, item.end).replace(/,/g, '');
-                }
-            }, '') + source.substr(lastIndex);
+                var startIndex = lastIndex;
+                lastIndex = end;
+
+                return {
+                    property_definition: _substr(startIndex, start) + _substr(start, end).replace(new RegExp('^' + _escapeRegular(key) + '$', 'gi'), '\'' + _escapeReplacement(key) + '\''),
+                    dot_access: _substr(startIndex, start - 1) + _substr(start - 1, end).replace(new RegExp('^\\.' + _escapeRegular(key) + '$', 'gi'), '[\'' + _escapeReplacement(key) + '\']'),
+                    trailing_comma: _substr(startIndex, start) + _substr(start, end).replace(/,/g, '')
+                }[type];
+            }).join('') + source.substr(lastIndex);
         }
     }, {
         key: 'apply',
         value: function apply(compiler) {
             var _this = this;
 
-            compiler.plugin('emit', function (compilation, callback) {
-                for (var fileName in compilation.assets) {
+            compiler.plugin('emit', function (_ref2, callback) {
+                var assets = _ref2.assets;
+
+                Object.entries(assets).forEach(function (_ref3) {
+                    var _ref4 = _slicedToArray(_ref3, 2),
+                        fileName = _ref4[0],
+                        asset = _ref4[1];
+
                     /** ignore map files */
-                    if (!/\.js$/gi.test(fileName)) continue;
+                    if (!/\.js$/gi.test(fileName)) return;
 
                     /** todo: how to modify map according to the source? */
 
-                    var _compilation$assets$f = compilation.assets[fileName].sourceAndMap(),
-                        source = _compilation$assets$f.source,
-                        map = _compilation$assets$f.map;
+                    var _asset$sourceAndMap = asset.sourceAndMap(),
+                        source = _asset$sourceAndMap.source,
+                        map = _asset$sourceAndMap.map;
 
-                    compilation.assets[fileName] = new SourceMapSource(_this.process(source, fileName), fileName, map, source, map);
-                }
+                    asset = new SourceMapSource(_this.process(source, fileName), fileName, map, source, map);
+                });
 
                 callback();
             });
